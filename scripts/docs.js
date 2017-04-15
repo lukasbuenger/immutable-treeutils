45d8e3b3f33ca52793c9ff5979d0e24e974d7c32
Lukas Bünger <lukas.buenger@fixxpunkt.ch>
2017-04-15 18:51:47 +0200

    new linting rules

--- scripts/docs.js
+++ scripts/docs.js
@@ -1,8 +1,9 @@
-import { parse } from 'acorn';
-import glob from 'glob';
-import fs from 'fs';
-import Mustache from 'mustache';
-import pkg from '../package';
+const { parse } = require('acorn');
+const glob = require('glob');
+const fs = require('fs');
+const Mustache = require('mustache');
+
+const pkg = require('../package');
 
 const tagExp = /@[a-z]+ (_|-|[a-zA-Z0-9]|\.)+/g;
 const asteriskAndNewLineExp = /(^\*)?\n( )?(\t+)? \*( )?/g;
@@ -11,27 +12,27 @@ const links = [
 	{
 		name: 'TreeUtils',
 		lookup: 'ImmutableJS',
-		url: 'http://github.com/lukasbuenger/immutable-treeutils/'
+		url: 'http://github.com/lukasbuenger/immutable-treeutils/',
 	},
 	{
 		name: 'ImmutableJS',
 		lookup: 'ImmutableJS',
-		url: 'http://facebook.github.io/immutable-js/'
+		url: 'http://facebook.github.io/immutable-js/',
 	},
 	{
 		name: 'Seq',
 		lookup: 'Immutable.Seq',
-		url: 'http://facebook.github.io/immutable-js/docs/#/Seq'
+		url: 'http://facebook.github.io/immutable-js/docs/#/Seq',
 	},
 	{
 		name: 'Record',
 		lookup: 'Immutable.Record',
-		url: 'http://facebook.github.io/immutable-js/docs/#/Record'
+		url: 'http://facebook.github.io/immutable-js/docs/#/Record',
 	},
 	{
 		name: 'List',
 		lookup: 'Immutable.List',
-		url: 'http://facebook.github.io/immutable-js/docs/#/List'
+		url: 'http://facebook.github.io/immutable-js/docs/#/List',
 	},
 
 ];
@@ -40,22 +41,24 @@ glob('src/*.js', {}, (er, fileNames) => {
 	if (er) {
 		console.error(er);
 	} else {
-		let refs = [],
-			parsed = fileNames.map(fileName => {
-				let data = fs.readFileSync(fileName, 'utf8'),
-					comments = '';
+		const refs = [];
+		const parsed = fileNames.map((fileName) => {
+			const data = fs.readFileSync(fileName, 'utf8');
+			let comments = '';
+			try {
 				parse(data, {
 					ecmaVersion: 6,
 					sourceType: 'module',
-					onComment: (block, text) => {
+					onComment: (block, commentText) => {
+						let text = commentText;
 						if (block) {
-							let attributes,
-								attributeMatch = tagExp.exec(text);
-							while(attributeMatch) {
+							let attributes;
+							let attributeMatch = tagExp.exec(text);
+							while (attributeMatch) {
 								if (!attributes) {
 									attributes = {};
 								}
-								let parts = attributeMatch[0].split(' ');
+								const parts = attributeMatch[0].split(' ');
 								attributes[parts[0].substr(1)] = parts[1];
 								attributeMatch = tagExp.exec(text);
 							}
@@ -66,59 +69,66 @@ glob('src/*.js', {}, (er, fileNames) => {
 							text = text.replace(asteriskAndNewLineExp, '\n');
 							comments = comments.concat(
 								'- - - \n',
-								attributes && attributes.id ? '<a id="' + attributes.id + '"></a>' : '',
+								attributes && attributes.id ? `<a id="${attributes.id}"></a>` : '',
 								text,
-								'\n\n'
+								'\n\n',
 							);
 							text = text.replace(tagExp, '');
 						}
-					}
+					},
 				});
-				return {
-					comments: comments,
-					fileName: fileName
-				};
-			});
-
-		let linkPatterns = links.reduce((memo, link) => {
-				memo[link.lookup] = '[' + link.name + '](' + link.url + ')';
-				return memo;
-			}, {}),
-			refPatterns = refs.reduce((memo, ref) => {
-				if (ref.id && ref.lookup) {
-					memo[ref.lookup] = '[' + (ref.name || ref.lookup) + '](#' + ref.id + ')';
-				}
-				return memo;
-			}, {});
-
-		let concatenatedComments = parsed.reduce((memo, item) => {
-			let comments = item.comments;
-
-			if (comments.length > 0) {
-
-				return memo.concat(
-					'- - - \n<sub>[See Source](',
-					'https://github.com/lukasbuenger/immutable-treeutils/tree/v' + pkg.version + '/' + item.fileName,
-					')</sub>\n',
-					comments
-				);
-			} else {
-				return memo;
+			} catch (e) {
+				throw (e.stack);
 			}
-		}, '');
-
-		let template = fs.readFileSync('scripts/README.mustache', 'utf8');
-		let rendered = Mustache.render(template, {
-				pkg: pkg,
-				docs: concatenatedComments
-			});
-
-		Object.keys(linkPatterns).forEach(key => {
-			rendered = rendered.replace(new RegExp('\>\\b' + key + '\\b', 'g'), linkPatterns[key]);
+			return {
+				comments,
+				fileName,
+			};
 		});
 
-		Object.keys(refPatterns).forEach(key => {
-			rendered = rendered.replace(new RegExp('\>\\b' + key + '\\b', 'g'), refPatterns[key]);
+		const linkPatterns = links.reduce((memo, link) => {
+			return Object.assign(
+				{},
+				memo,
+				{ [link.lookup]: `[${link.name}](${link.url})` },
+			);
+		}, {});
+		const refPatterns = refs.reduce((memo, ref) => {
+			if (ref.id && ref.lookup) {
+				return Object.assign(
+					{},
+					memo,
+					{ [ref.lookup]: `[${ref.name || ref.lookup}](#${ref.id})` },
+				);
+			}
+			return memo;
+		}, {});
+
+		const concatenatedComments = parsed.reduce((memo, item) => {
+			const comments = item.comments;
+
+			if (comments.length > 0) {
+				return memo.concat(`
+					- - - \n<sub>[See Source](https://github.com/lukasbuenger/immutable-treeutils/tree/v${pkg.version}/${item.fileName},
+					)</sub>`,
+					comments,
+				);
+			}
+			return memo;
+		}, '');
+
+		const template = fs.readFileSync('scripts/README.mustache', 'utf8');
+		let rendered = Mustache.render(template, {
+			pkg,
+			docs: concatenatedComments,
+		});
+
+		Object.keys(linkPatterns).forEach((key) => {
+			rendered = rendered.replace(new RegExp(`>\\b${key}\\b`, 'g'), linkPatterns[key]);
+		});
+
+		Object.keys(refPatterns).forEach((key) => {
+			rendered = rendered.replace(new RegExp(`>\\b${key}\\b`, 'g'), refPatterns[key]);
 		});
 
 		fs.writeFileSync('README.md', rendered);
