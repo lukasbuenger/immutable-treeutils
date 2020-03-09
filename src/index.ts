9bba4f04135a8c14544618d67d0e7de81c462d9c
Lukas Buenger <lukasbuenger@gmail.com>
2020-03-09 10:36:57 +0100

    shorter exports

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,7 @@
 export * from './types'
-export { PreOrder, ReversePreOrder } from './traversal/preorder'
-export { PostOrder, ReversePostOrder } from './traversal/postorder'
-export { InOrder, ReverseInOrder } from './traversal/inorder'
-export { BFS, ReverseBFS } from './traversal/bfs'
-export {
-  ancestors,
-  childAt,
-  childIndex,
-  childNodes,
-  depth,
-  descendants,
-  filter,
-  find,
-  findId,
-  firstChild,
-  firstDescendant,
-  getId,
-  hasChildNodes,
-  lastChild,
-  lastDescendant,
-  left,
-  nextSibling,
-  nodes,
-  numChildNodes,
-  parent,
-  position,
-  previousSibling,
-  reduceTree,
-  resolve,
-  right,
-  siblings,
-} from './methods'
-
-export {
-  defaultOptions,
-  defaultMethods,
-  APIFactory,
-  TreeUtils,
-} from './lib'
+export * from './traversal/preorder'
+export * from './traversal/postorder'
+export * from './traversal/inorder'
+export * from './traversal/bfs'
+export * from './methods'
+export * from './lib'
