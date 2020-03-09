a5ac89424ab70802908d78aa733ec2e80d5d3236
Lukas Buenger <lukasbuenger@gmail.com>
2020-03-09 14:03:14 +0100

    cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,14 @@
 module.exports = {
-  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  parser: '@typescript-eslint/parser',
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
-  plugins: ['react-hooks'],
+  plugins: ['@typescript-eslint'],
   parserOptions: {
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
   env: {
     browser: true,
@@ -28,20 +27,5 @@ module.exports = {
     ],
     'no-undef': 'off',
     'no-shadow': 'error',
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'react/display-name': 'off',
-    'react/jsx-uses-react': 'error',
-    'react/jsx-uses-vars': 'error',
-    'react/jsx-no-undef': 'error',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'error',
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-  },
-  settings: {
-    react: {
-      version: 'latest', // Tells eslint-plugin-react to automatically detect the version of React to use
-    },
   },
 }
